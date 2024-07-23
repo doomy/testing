72c48f95badd87c6184b86f1499a0467473bddce
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-07-23 15:52:30 +0200

    Dockerfile path fixed

--- bin/tests-up.sh
+++ bin/tests-up.sh
@@ -1,4 +1,5 @@
-docker build -t test-db .
+script_dir=$(dirname "$0")
+docker build -t test-db "$script_dir/.."
 container_id=$(docker run -d -p 3999:3306 --name test-db test-db)
 docker wait "$container_id"
 echo "container up"

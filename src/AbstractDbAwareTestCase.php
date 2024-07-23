45f4635215f8f0d7d685ef227ade231ecf2f0d48
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-07-23 23:40:08 +0200

    AbstractDbAwareTestCase

---
+++ src/AbstractDbAwareTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doomy\Testing;
+
+use Dibi\Connection;
+use PHPUnit\Framework\TestCase;
+
+abstract class AbstractDbAwareTestCase extends TestCase
+{
+    protected Connection $connection;
+
+    public function __construct(string $name)
+    {
+        $rawConfig = file_get_contents(__DIR__ . '/../testingDbCredentials.json');
+        if ($rawConfig === false) {
+            throw new \Exception('Could not read config');
+        }
+        $config = json_decode($rawConfig, true);
+        if (! is_array($config)) {
+            throw new \Exception('Invalid config');
+        }
+        $this->connection = new Connection($config);
+        parent::__construct($name);
+    }
+}
